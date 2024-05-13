bb632df5f43df6881ab56da10ab409756c08a8a3
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-05-13 19:43:45 +0100

    navbar

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+import './globals.css'
+
+function App() {
+
+
+  return (
+    <div className="bg-white bodyBG">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='flex md:items-end md:justify-end items-center justify-between px-6 mt-4'>
+            <img
+            src='./assets/logo.svg'
+            className='md:pt-4 md:flex'
+            />
+
+            <div className='flex px-2 space-x-4 md:space-x-10 md:mx-12 '>
+                <p className=' font-inter text-[16px] md:text-[24px]'>Portfolio</p>
+                <div className='flex flex-cpl'>
+                    <p className=' font-inter text-[16px] md:text-[24px]'>Hire Me</p>
+                    <div className=' border-b bottom-3 border-pink-300'></div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
